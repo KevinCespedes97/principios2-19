e2008d30aacb8667cb89f22002aeb84f1e9370e1
Jose Antonio Ciccio <jciccio@gmail.com>
2019-08-21 22:37:01 -0600

    Comentarios agregados a clase carta
    Comentarios agregados a clase carta
    
    Signed-off-by: Jose Antonio Ciccio <jciccio@gmail.com>

--- Semana2Carta.py
+++ Semana2Carta.py
@@ -1,33 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class Carta:
+class Carta: #Declaración de una clase, usando la palabra reservada class
 
-	#Vamos a crear un metodo constructor que se encarga de definir los atributos
+	
+	#Los elementos indentados (con sangría a este nivel) serán parte de la clase
+
+	#Constructor de clase, la acción __init__ es llamada sólo una vez
+	#Cuando se crea una nueva instancia de la clase
+	#Entre los paréntesis se definen las variables que se desean recibir al
+	#Crear un objeto y usarlas para dar valor a los atributos
 	def __init__(self, numeroDeCarta, colorCarta, simbolo):
-		self.numero = numeroDeCarta
-		self.color = colorCarta
+		#Los elementos con sangría a este nivel serán parte del método __init__
+		self.numero = numeroDeCarta #Se crea un atributo numero usando la sintaxis self.X
+		self.color = colorCarta #Se crea e inicializa el atributo color
 		self.simbolo = simbolo
 
 
 	def imprimir(self):
 		print("La carta es: " + str(self.numero) + " de " + self.simbolo)
+		#Recuerde que para concatenar (unir) Strings, todos los elementos
+    	#Deben ser del mismo tipo por lo que convertimos el numero en un String
 
 
 	def asignarNumero(self, valor):
 		self.numero = valor
 
+	#En algunas ocasiones, queremos que los métodos retornen un valor.
+	#Por ejemplo el siguiente metodo retorna el valor de un numero
+	#Para retornar, se utiliza la palabra reservada return seguido de lo que queremos retornar
+	def obtenerNumero(self):
+		return self.numero
 
 
 
 
 
-#Creacion de una instancia de la clase Carta
+
+#Creacion de instancias de la clase Carta
+#Note que esto no es parte de la clase, por tanto la indentación (Sangría)
+#Vuelve a la izquierda
 carta1 = Carta(4, "negro" , "espadas")
 carta1.imprimir()
 
 carta2 = Carta(3, "rojo" , "corazones")
 carta2.imprimir()
+#Para una instancia existente, se pueden sobreescribir los valores de los atributos:
 carta2.asignarNumero(9)
 carta2.imprimir()
 #nombre de variable = nombre de la clase
+valorDeLaCarta2 = carta2.obtenerNumero()
+print("El numero de la instancia carta2 es: " + str(valorDeLaCarta2))
